d83e38aa896c1382e7306c0cdad456d7c1ac3e22
Edwin Nemeguen <95711147+PhoenixDwarf@users.noreply.github.com>
2025-07-07 20:36:50 -0500

    feat: implement dynamic routing in navbar and update page title display

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,8 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { NavigationEnd, Router, RouterLink } from '@angular/router';
+import { routes } from '../../../app.routes';
+import { filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './navbar.component.html',
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  router = inject(Router);
+
+  routes = routes
+    .map((route) => ({
+      path: route.path,
+      title: `${route.title ?? 'Maps in Angular'}`,
+    }))
+    .filter((route) => route.path !== '**');
+
+  pageTitle = toSignal(
+    this.router.events.pipe(
+      filter((event) => event instanceof NavigationEnd),
+      map((event) => event.url),
+      map(
+        (url) =>
+          routes.find((route) => `/${route.path}` === url)?.title ||
+          'Maps in Angular'
+      )
+    )
+  );
+}
