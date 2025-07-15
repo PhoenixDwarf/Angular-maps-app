0e0f5901a8c9115ceb85a8d066f5ff477bd44a82
Edwin Nemeguen <95711147+PhoenixDwarf@users.noreply.github.com>
2025-07-14 21:10:32 -0500

    feat: implement markers page with Mapbox integration and initial map setup

--- src/app/pages/markers-page/markers-page.component.ts
+++ src/app/pages/markers-page/markers-page.component.ts
@@ -1,8 +1,49 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  signal,
+  viewChild,
+} from '@angular/core';
+import mapboxgl from 'mapbox-gl';
+import { environment } from '../../../environments/environment';
+
+mapboxgl.accessToken = environment.mapboxKey;
 
 @Component({
   selector: 'app-markers-page',
   imports: [],
   templateUrl: './markers-page.component.html',
 })
-export class MarkersPageComponent { }
+export class MarkersPageComponent implements AfterViewInit {
+  divElement = viewChild<ElementRef>('map');
+  map = signal<mapboxgl.Map | null>(null);
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      const element = this.divElement()!.nativeElement;
+
+      const map = new mapboxgl.Map({
+        container: element, // container ID
+        style: 'mapbox://styles/mapbox/streets-v12', // style URL
+        center: [-74.0948, 4.6617], // starting position [lng, lat]
+        zoom: 14, // starting zoom
+      });
+
+      const marker = new mapboxgl.Marker({
+        draggable: false,
+        color: '#000',
+      })
+        .setLngLat([-74.0948, 4.6617])
+        .addTo(map);
+
+      marker.on('dragend', (event) => console.log('Marker dragged', event));
+
+      this.mapListeners(map);
+    }, 80);
+  }
+
+  mapListeners(map: mapboxgl.Map) {
+    console.log('object');
+  }
+}
